37db922d088d0434e377f3751350ded550d625ee
faizanagha <faizan.agha@gmail.com>
2015-02-13 11:06:10 +0000

    first-commit

---
+++ NetBeansProjects/FirstGitPush/src/firstgitpush/FirstGitPush.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstgitpush;
+
+/**
+ *
+ * @author dmsc1401
+ */
+public class FirstGitPush {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Program just to pust into GitHub");
+    }
+}
